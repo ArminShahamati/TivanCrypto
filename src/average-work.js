a6a9c6df3083505f1537fcf868a2b7f8572baf9f
akbar shahamati <akbar.shahamati@live.com>
2021-05-02 23:18:24 +0430

    Improve the Proof of Work System  Average Work Script

---
+++ src/average-work.js
@@ -0,0 +1,25 @@
+const Blockchain = require("./blockchain");
+const blockchain = new Blockchain();
+
+blockchain.addBlock({ data: "initial" });
+
+let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
+
+const times = [];
+
+for (let i = 0; i < 10000; i++) {
+  prevTimestamp = blockchain.chain[blockchain.chain.length - 1].timeStamp;
+
+  blockchain.addBlock({ data: `block ${i}` });
+  nextBlock = blockchain.chain[blockchain.chain.length - 1];
+
+  nextTimestamp = nextBlock.timeStamp;
+  timeDiff = nextTimestamp - prevTimestamp;
+  times.push(timeDiff);
+
+  average = times.reduce((total, num) => total + num) / times.length;
+
+  console.log(
+    `Time to mine block : ${timeDiff}ms. Difficulty : ${nextBlock.difficulty}. Average time : ${average}ms`
+  );
+}
