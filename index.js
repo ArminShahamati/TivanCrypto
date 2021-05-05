522c932316a90164741f49280b74e6b75a7cc9e3
akbar shahamati <akbar.shahamati@live.com>
2021-05-05 23:08:56 +0430

    Start Peers and Broadcast Chain on API Mine - Add Cross Config to Miner API

--- index.js
+++ index.js
@@ -22,10 +22,19 @@ app.post("/api/mine", (req, res) => {
 
   blockchain.addBlock({ data });
 
+  pubsub.broadcastChain();
+
   res.redirect("/api/blocks");
 });
 
-const PORT = 3000;
+const DEFAULT_PORT = 3000;
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === "true") {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
   console.log(`start server on port : ${PORT}`);
 });
